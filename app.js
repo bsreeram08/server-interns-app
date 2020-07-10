75bdf057a1852618a60c4ceb8112fe3dae11b4f4
Sreeram Balamurugan <bsreeram08@gmail.com>
2020-07-10 13:25:25 +0530

    first commit3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const loginRouter = require("./routers/loginRouter.js");
 const assignmentRouter = require("./routers/assignmentRouter.js");
 const videosRouter = require("./routers/videosRouter");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const host = "127.0.0.1";
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
